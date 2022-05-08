a236bb00848eb11562c9c5c40975b69ad95d5ad9
Hiramon Nahar Eva <hiramonnahareva@gmail.com>
2022-05-08 15:58:13 +0600

    edit header

--- src/Pages/Common/Header/Header.js
+++ src/Pages/Common/Header/Header.js
@@ -27,12 +27,11 @@ const Header = () => {
            <li className='mx-4 my-6 md:my-0'> <CustomLink className='text-xl hover:text-amber-700 duration-500' to='/home'>HOME</CustomLink> </li>
            <li className='mx-4 my-6 md:my-0'> <CustomLink className='text-xl hover:text-amber-700 duration-500' to='/inventory'>INVENTORY</CustomLink> </li>
            <li className='mx-4 my-6 md:my-0'> <CustomLink className='text-xl hover:text-amber-700 duration-500' to='/blogs'>BLOGS</CustomLink> </li>
-           <li className='mx-4 my-6 md:my-0'> <CustomLink className='text-xl hover:text-amber-700 duration-500' to='/item/:id'>MANAGE ITEM</CustomLink></li>
            <li className='my-6 md:my-0'>
                {
                    user && <ul className={`md:flex md:items-center `}>
                     <li className='mx-4 my-6 md:my-0'> <CustomLink className='text-xl hover:text-amber-700 duration-500' to='/abbNew'>ADD ITEM</CustomLink></li>
-                    {/* <li className='mx-4 my-6 md:my-0'> <CustomLink className='text-xl hover:text-amber-700 duration-500' to='/items/:itemId'>MANAGE ITEM</CustomLink></li> */}
+                    <li className='mx-4 my-6 md:my-0'> <CustomLink className='text-xl hover:text-amber-700 duration-500' to='/item/:id'>MANAGE ITEM</CustomLink></li>
                    </ul>
                }
                  </li>
